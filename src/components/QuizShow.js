d8dbf1b8bcb5e83e8690d3e22c2bbc2814ff18f2
Toni Langley <tonia.langley@gmail.com>
2021-07-25 17:03:26 -0400

    Display questions
    - Add fetch to store question state
    - Render questions component with above state

--- src/components/QuizShow.js
+++ src/components/QuizShow.js
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import Questions from './Questions'
 
 function QuizShow () {
     const location = useLocation()
     const { quiz } = location.state
 
+    const [questions, setQuestions] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:3000/quizzes/${quiz.id}/questions`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${localStorage.token}`
+            }
+        })
+        .then(res => res.json())
+        .then(questions => {
+            setQuestions(questions)
+        })
+    }, [])
+
     return (
         <div>
             <h3>{quiz.name}</h3>
             <h4>{quiz.description}</h4>
+            <Questions questions={questions}/>
         </div>
     )
 }
